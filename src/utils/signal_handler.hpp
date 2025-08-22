419771ac9ab9c6c9cc307d525486d5786017c938
Swordmaster3214 <118647835+Swordmaster3214@users.noreply.github.com>
2025-08-21 19:23:39 -0500

    Create signal_handler.hpp

---
+++ src/utils/signal_handler.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <atomic>
+#include <signal.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+class SignalHandler {
+public:
+    static void setup();
+    static bool should_exit();
+    
+private:
+    static std::atomic<bool> exit_requested_;
+    
+#ifdef _WIN32
+    static BOOL WINAPI console_ctrl_handler(DWORD dwCtrlType);
+#else
+    static void handle_sigint(int);
+#endif
+};
+
+extern std::atomic<bool> g_running;
